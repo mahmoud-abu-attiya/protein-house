d45b901fa9e6a63de47314dbbad4249afbe34d25
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2023-05-25 18:21:28 +0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "#382C44",
+      secondary: "#FFD0BF",
+    },
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+}
+
